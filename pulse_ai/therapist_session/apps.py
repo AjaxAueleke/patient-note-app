660b7c11dcfb135e037f430531f5e5553260346d
Muhammad Ahmed <ahmed.jamil7410@gmail.com>
2024-04-14 09:38:37 +0500

    therapist sessions completed
    Signed-off-by: Muhammad Ahmed <ahmed.jamil7410@gmail.com>

--- pulse_ai/therapist_session/apps.py
+++ pulse_ai/therapist_session/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TherapistSessionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'therapist_session'
+    name = 'pulse_ai.therapist_session'
