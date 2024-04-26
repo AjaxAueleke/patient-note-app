60ea125bb9ed0434c5c790b2c60ae27c10cf1cd4
Muhammad Ahmed <ahmed.jamil7410@gmail.com>
2024-04-26 05:18:40 +0500

    changes
    Signed-off-by: Muhammad Ahmed <ahmed.jamil7410@gmail.com>

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,9 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [{"NAME": "django.contrib.auth.password_validation.Us
 # MIDDLEWARE
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#middleware
-MIDDLEWARE = ["pulse_ai.users.middleware.RequestLoggingMiddleware", "django.middleware.security.SecurityMiddleware",
-              "corsheaders.middleware.CorsMiddleware", "django.contrib.sessions.middleware.SessionMiddleware",
-              "django.middleware.locale.LocaleMiddleware", "django.middleware.common.CommonMiddleware",
+MIDDLEWARE = ["pulse_ai.users.middleware.RequestLoggingMiddleware",
+              "django.middleware.security.SecurityMiddleware",
+              "corsheaders.middleware.CorsMiddleware",
+              # "django.contrib.sessions.middleware.SessionMiddleware",
+              "django.middleware.locale.LocaleMiddleware",
+              "django.middleware.common.CommonMiddleware",
               # "django.middleware.csrf.CsrfViewMiddleware",
               "django.contrib.auth.middleware.AuthenticationMiddleware",
               "django.contrib.messages.middleware.MessageMiddleware",
