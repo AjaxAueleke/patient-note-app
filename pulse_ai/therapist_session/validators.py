d2170cf15d9175bd97eca8f30ced1e01fcf8449f
sufyan imran <syedsufyan36@gmail.com>
2024-08-28 14:38:00 +0500

    audio file aceept all

--- pulse_ai/therapist_session/validators.py
+++ pulse_ai/therapist_session/validators.py
@@ -2,9 +2,10 @@ import magic
 from django.core.exceptions import ValidationError
 
 def validate_audio_mime_type(value):
-    mime = magic.Magic(mime=True)
-    mime_type = mime.from_buffer(value.file.read(2048))  # Read the first few bytes to determine MIME type
-    if not mime_type.startswith('audio'):
-        raise ValidationError('This file is not an audio file.')
-    value.file.seek(0)  # Reset file pointer after reading
+    return True
+    # mime = magic.Magic(mime=True)
+    # mime_type = mime.from_buffer(value.file.read(2048))  # Read the first few bytes to determine MIME type
+    # if not mime_type.startswith('audio'):
+    #     raise ValidationError('This file is not an audio file.')
+    # value.file.seek(0)  # Reset file pointer after reading
 
